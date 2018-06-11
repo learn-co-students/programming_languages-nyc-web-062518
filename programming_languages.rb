ee760238a8d1a146fa2cee49ac1804db1783f239
yunglleung1 <yungl.leung1@gmail.com>
2018-06-11 12:16:46 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
 def reformat_languages(languages)
   # your code here
+  arrayOfStyles = []
+  new_hash = {}
+  languages.each do |languageCategory, languageSet|
+    languageSet.each do |languageItem, type|
+      if languageItem != :javascript
+        type[:style] = [languageCategory]
+        new_hash[languageItem] = type
+      else
+        arrayOfStyles.push(languageCategory)
+        new_hash[languageItem] = type
+      end
+    end
+  end
+  new_hash[:javascript][:style] = arrayOfStyles
+  return new_hash
 end
