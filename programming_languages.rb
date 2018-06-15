d10de30b497b112d5c5d9d52ceb5d172d4370ede
grandballoon <luke.sovern.conley@gmail.com>
2018-06-15 11:54:52 -0400

    programming languages

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,24 @@
+require "pry"
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |type, collection|
+  collection.each do |language, type_hash|
+    if !new_hash[language]
+      new_hash[language] = type_hash
+    end
+      if language == :javascript
+        #binding.pry
+      end
+      if new_hash[language][:style] == nil
+      new_hash[language][:style] = [type]
+      else
+      new_hash[language][:style] << type
+    end
+    end
+    #new_hash.each do |idiom, values|
+    #  new_hash[idiom].store(:style, type)
+    #end
+  end
+
+  new_hash
 end
