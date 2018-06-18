f222dad9ff6dad88cbcd6ea36021cc7810c5e4a7
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-06-18 16:56:19 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  languages_reorg = {}
+  languages.each do |style, language_hash|
+    language_hash.each do |language, type_hash|
+      if languages_reorg[language] == nil
+        languages_reorg[language] = type_hash
+        languages_reorg[language][:style] = [style]
+      else languages_reorg[language][:style] << style
+      end
+    end
+  end
+  languages_reorg
 end
